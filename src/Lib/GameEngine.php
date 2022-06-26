9c63ab3323e39ffa834981f46be6ba7310eb4aa3
Daniel Ribes <daniel@danielribes.com>
2022-06-26 20:43:29 +0200

    renamed the Game class to GameEngine

---
+++ src/Lib/GameEngine.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This class setup the game and have the rules
+ */
+
+namespace SnakesAndLadders\Lib;
+
+use SnakesAndLadders\Lib\Player;
+
+class GameEngine 
+{
+    public $token;
+
+    public function __construct() 
+    {
+        $this->player = new Player();          
+    }
+
+    public function checkPlayer($player)
+    {
+        $position = $player->getActualSquare();
+        if($position == 100)
+        {
+            $player->setWin();
+        }
+
+        if($position > 100)
+        {
+            $player->moveToSquare($player->getOldPosition());
+        }
+    }
+
+}
